227a56da5583fb68eff9dfd24d269245158e9225
Benjamin Vandenbussche <benjvdb9@hotmail.com>
2016-02-03 15:53:35 +0100

    Add unit test for utils module

--- utils.py
+++ utils.py
@@ -38,9 +38,9 @@ def integrate(function, lower, upper):
 
 class TestUtils(unittest.TestCase):
     def test_fact(self):
-        #self.assertEqual(fact(0), 0)
-        #self.assertEqual(fact(3), 6)
-        #self.assertEqual(fact(5), 120)
+        self.assertEqual(fact(0), 0)
+        self.assertEqual(fact(3), 6) #Git regn,i
+        self.assertEqual(fact(5), 120)
         with self.assertRaises(ValueError):
             fact(-1)
         pass
